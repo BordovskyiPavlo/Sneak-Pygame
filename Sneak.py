06c5b2b9bf11388f01d30e78d2a01ac54da56dbd
BordovskyiPavlo <99017519+BordovskyiPavlo@users.noreply.github.com>
2022-02-04 14:48:54 +0200

    Add files via upload

--- Sneak.py
+++ Sneak.py
@@ -1,58 +1,47 @@
 import pygame , random
 from pygame.locals import *
 pygame.init()
-col , row ,size = 16 , 16 ,50
+col ,row ,size = 16 , 16 ,50
 time = 3
 gamescreen = pygame.display.set_mode((col*size,row*size))
 pygame.display.set_caption("Hello Sneak")
 clock = pygame.time.Clock()
 background = pygame.Surface((col*size,row*size))
 background.fill((255,255,255))
+online = True
+LEFT,RIGHT,UP,DOWN = 1,2,3,4
+direction = RIGHT
+update = direction
+body = [(col//2, row//2)]
+step = (1,0)
+length = 1
 for i in range (1, col):
     pygame.draw.line(background,(128,128,128),(i*size-1,0),(i*size-1,row*size),2)
 for i in range (1, row):
     pygame.draw.line(background,(128,128,128),(0,i*size-1),(col*size,i*size-1),2)
+
 def random_position(body):
     while True:
         position = random.randrange(col),random.randrange(row)
         if position not in body:
             break
-    return  position
-body = [(col//2, row//2)]
-step = (1,0)
-length = 1
+    return position
 food = random_position(body)
-time = 3
-online = True
+def eat_food(length,time):
+    length += 1
+    time += 0.5
+
+def spawn_food(body):
+    global food
+    food = random_position(body)
+
+
 def game_over(body):
     for i in range(1,length):
         if body[0] == body[i]:
             pygame.quit()
-while online:
-    clock.tick(time)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            online = False
-        if event.type == pygame.KEYDOWN:
-            permission = {"a": True, "d": True, "w": True, "s": True}
-            if event.key == pygame.K_a and permission["a"]:
-                permission = {"a": True, "d": False, "w": True, "s": True}
-                step = (-1, 0)
-            if event.key == pygame.K_d and permission["d"]:
-                permission = {"a": False, "d": True, "w": True, "s": True}
-                step = (1, 0)
-            if event.key == pygame.K_w and permission["w"]:
-                permission = {"a": True, "d": True, "w": True, "s": False}
-                step = (0, -1)
-            if event.key == pygame.K_s and permission["s"]:
-                permission = {"a": True, "d": True, "w": False, "s": True}
-                step = (0, 1)
-    body.insert(0,body[0][:])
-    body[0] = (body[0][0] + step[0]) % col, (body[0][1] + step[1]) % row
-    if body[0] == food:
-        food = random_position(body)
-        length+= 1
-        time+= 0.5
+
+def control_length(body):
     while len(body) > length:
         del body[-1]
         gamescreen.blit(background,(0,0))
@@ -62,3 +51,41 @@ while online:
             pygame.draw.rect(gamescreen, color, (pos[0] * size, pos[1] * size, size, size))
         game_over(body)
         pygame.display.flip()
+
+while online:
+    clock.tick(time)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            online = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a :
+                update = LEFT
+            elif event.key == pygame.K_d:
+                update = RIGHT
+            elif event.key == pygame.K_w:
+                update = UP
+            elif event.key == pygame.K_s :
+                update = DOWN
+            if update == LEFT and direction != RIGHT:
+                direction = LEFT
+            if update == RIGHT and direction != LEFT:
+                direction = RIGHT
+            if update == UP and direction != DOWN:
+                direction = UP
+            if update == DOWN and direction != UP:
+                direction = DOWN
+            if direction == LEFT:
+                step = (-1,0)
+            if direction == RIGHT:
+                step = (1,0)
+            if direction == UP:
+                step = (0,-1)
+            if direction == DOWN:
+                step = (0,1)
+    body.insert(0,body[0][:])
+    body[0] = (body[0][0] + step[0]) , (body[0][1] + step[1])
+    if body[0] == food:
+        food = random_position(body)
+        length+= 1
+        time+= 0.5
+    control_length(body)
