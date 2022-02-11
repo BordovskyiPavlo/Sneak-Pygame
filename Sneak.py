b6e4018839a29ea00f57373f3688ce3330b38997
BordovskyiPavlo <99017519+BordovskyiPavlo@users.noreply.github.com>
2022-02-11 13:11:29 +0200

    Add files via upload

--- Sneak.py
+++ Sneak.py
@@ -1,5 +1,7 @@
 import pygame , random
 from pygame.locals import *
+def __main__():
+    return
 pygame.init()
 col ,row ,size = 15 , 15 ,40
 time = 3
@@ -8,14 +10,14 @@ time = 3
 gamescreen = pygame.display.set_mode((col*size,row*size+25))
 background = pygame.Surface((col*size,row*size))
 background.fill((255,255,255))
-
+GameOver = False
 pygame.display.set_caption("Hello Sneak")
 clock = pygame.time.Clock()
 online = True
 gameover = False
 White = (255,255,255)
 Black = (0,0,0)
-onlineupdate = True
+
 #direction
 LEFT,RIGHT,UP,DOWN = 1,2,3,4
 direction = RIGHT
@@ -25,6 +27,8 @@ score=1
 body = [(col//2, row//2)]
 step = (1,0)
 length = 1
+
+A = (body[0][0],-1)
 
 #Score
 font = pygame.font.SysFont(None, 25)
@@ -47,10 +51,6 @@ food = random_position(body)
 def spawn_food(body):
     global food
     food = random_position(body)
-def game_over(body):
-    for i in range(1,length):
-        if body[0] == body[i]:
-            pygame.quit()
 
 def control_length(body):
     while len(body) > length:
@@ -60,10 +60,13 @@ def control_length(body):
         for i, pos in enumerate(body):
             color = (255, 0, 0) if i == 0 else (0, 192, 0) if (i % 2) == 0 else (255, 128, 0)
             pygame.draw.rect(gamescreen, color, (pos[0] * size, pos[1] * size, size, size))
-        game_over(body)
         pygame.display.flip()
 while online:
     clock.tick(time)
+    for i in range(1, length):
+        if body[0] == body[i]:
+            GameOver = True
+            online = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             online = False
@@ -98,6 +101,18 @@ while online:
     body[0] = (body[0][0] + step[0]), (body[0][1] + step[1])
     pygame.display.update()
     font = pygame.font.SysFont(None, 25)
+    if body[0] == (-1,body[0][1]):
+        online = False
+        GameOver = True
+    if body[0] == (body[0][0],-1):
+        online = False
+        GameOver = True
+    if body[0] == (15,body[0][1]):
+        online = False
+        GameOver = True
+    if body[0] == (body[0][0],15):
+        online = False
+        GameOver= True
     if body[0] == food:
         text = font.render("Score: " + str(length), True, Black)
         gamescreen.blit(text, (5, 600))
@@ -111,3 +126,24 @@ while online:
     gamescreen.blit(text, (5, 600))
     pygame.display.update()
     control_length(body)
+while GameOver:
+    background.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            GameOver = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+            if event.key == pygame.K_SPACE:
+                GameOver = False
+                online = True
+                main()
+    font = pygame.font.SysFont(None, 50)
+    text1 = font.render("GAME OVER", True, Black)
+    font = pygame.font.SysFont(None, 30)
+    text2 = font.render("If you want to restart the game press SPACE", True, Black)
+    text3 = font.render("otherwise esc to exit", True, Black)
+    gamescreen.blit(text1, ((col*size)/2-100, (row*size)/2-100))
+    gamescreen.blit(text2, ((col*size)/2-150, (row*size)/2))
+    gamescreen.blit(text3, ((col * size)/2-150, (row * size)/2+30))
+    pygame.display.update()
